d404707112904ddc38cbb96838a4926a02c4bcd5
jkhask <john.haskell@slalom.com>
2024-01-12 12:38:34 -0500

    static content added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import { Typography } from '@mui/joy'
 import Container from '@mui/joy/Container'
 import CssBaseline from '@mui/joy/CssBaseline'
+import Sheet from '@mui/joy/Sheet'
+import Typography from '@mui/joy/Typography'
 import { CssVarsProvider } from '@mui/joy/styles'
+import MainCard, { MainCardProps } from './components/MainCard'
 
 export default function App() {
+  const cards: MainCardProps[] = [
+    {
+      title: 'Dev',
+      imgUrl: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159',
+    },
+    {
+      title: 'Mix',
+      imgUrl: 'https://images.unsplash.com/photo-1594623930572-300a3011d9ae',
+    },
+    {
+      title: 'Shred',
+      imgUrl: 'https://images.unsplash.com/photo-1418662589339-364ad47f98a2',
+    },
+    {
+      title: 'Smash',
+      imgUrl: 'https://images.unsplash.com/photo-1494666144702-70d86c6cf884',
+    },
+    {
+      title: 'Home',
+      imgUrl: 'https://images.unsplash.com/photo-1562762394-3acfba62a48e',
+    },
+    {
+      title: 'Roots',
+      imgUrl: 'https://images.unsplash.com/photo-1503152394-c571994fd383',
+    },
+  ]
+
   return (
     <CssVarsProvider defaultMode="dark">
       <CssBaseline />
       <Container
-        sx={theme => ({
+        sx={{
           minHeight: '100vh',
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
           py: 4,
-        })}>
+        }}
+      >
         <Typography
           level="h1"
-          fontWeight="xl"
-          fontSize="clamp(1.875rem, 1.3636rem + 2.1818vw, 3rem)">
+          sx={{
+            fontWeight: 'xl',
+            fontSize: 'clamp(1.875rem, 1.3636rem + 2.1818vw, 3rem)',
+            mb: 4,
+          }}
+        >
           John Haskell
         </Typography>
+        <Sheet
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: 4,
+            p: 4,
+            justifyContent: 'center',
+          }}
+        >
+          {cards.map(({ title, imgUrl }, i) => (
+            <MainCard title={title} imgUrl={imgUrl} key={i}></MainCard>
+          ))}
+        </Sheet>
       </Container>
     </CssVarsProvider>
   )
--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,5 +1,40 @@
 import Card from '@mui/joy/Card'
+import CardContent from '@mui/joy/CardContent'
+import CardCover from '@mui/joy/CardCover'
+import Typography from '@mui/joy/Typography'
 
-export default function MainCard() {
-  return <Card color="neutral" variant="soft" />
+export interface MainCardProps {
+  title: string
+  imgUrl: string
+  width?: number
+}
+
+export default function MainCard({
+  title,
+  imgUrl,
+  width = 320,
+}: MainCardProps) {
+  return (
+    <Card color="neutral" variant="outlined" sx={{ height: 280, width }}>
+      <CardCover>
+        <img
+          src={`${imgUrl}?auto=format&fit=crop&w=${width}"`}
+          loading="lazy"
+          alt={title}
+        />
+      </CardCover>
+
+      <CardCover
+        sx={{
+          background:
+            'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
+        }}
+      />
+      <CardContent sx={{ justifyContent: 'flex-end' }}>
+        <Typography level="title-lg" textColor="#fff">
+          {title}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 }
