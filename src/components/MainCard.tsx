321efa1ca2b8b2644314a88ec986fd80ff755ad4
jkhask <john.haskell@slalom.com>
2024-01-12 14:06:43 -0500

    cool hover

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,21 +1,38 @@
 import Card from '@mui/joy/Card'
 import CardContent from '@mui/joy/CardContent'
 import CardCover from '@mui/joy/CardCover'
+import Link from '@mui/joy/Link'
 import Typography from '@mui/joy/Typography'
+import { useState } from 'react'
 
 export interface MainCardProps {
   title: string
   imgUrl: string
   width?: number
+  height?: number
 }
 
 export default function MainCard({
   title,
   imgUrl,
   width = 320,
+  height = 280,
 }: MainCardProps) {
+  const [isHovering, setIsHovering] = useState(false)
   return (
-    <Card color="neutral" variant="outlined" sx={{ height: 280, width }}>
+    <Card
+      color="neutral"
+      variant="outlined"
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      sx={theme => ({
+        height,
+        width,
+        boxShadow: isHovering ? theme.shadow.lg : 'none',
+        '--joy-shadowChannel': theme.vars.palette.primary.mainChannel,
+        '--joy-shadowRing': 'inset 0 -3px 0 rgba(255 255 255 / 0.24)',
+      })}
+    >
       <CardCover>
         <img
           src={`${imgUrl}?auto=format&fit=crop&w=${width}"`}
@@ -29,11 +46,19 @@ export default function MainCard({
           background:
             'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
         }}
+        onMouseOver={e => console.log(e)}
       />
       <CardContent sx={{ justifyContent: 'flex-end' }}>
-        <Typography level="title-lg" textColor="#fff">
-          {title}
-        </Typography>
+        <Link
+          overlay
+          underline="none"
+          href="#interactive-card"
+          sx={{ color: 'text.tertiary' }}
+        >
+          <Typography level="title-lg" textColor="#fff">
+            {title}
+          </Typography>
+        </Link>
       </CardContent>
     </Card>
   )
